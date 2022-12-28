6a7004eb800ee1f66c7edb530679c7f7afab68d3
sirusalazar <sirusalazar@gmail.com>
2022-12-28 17:13:14 -0500

    removed unused module

--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorComponent } from './paginator/paginator.component';
-import { NgxPaginationModule } from 'ngx-pagination';
 
 const components = [PaginatorComponent];
 
 @NgModule({
   declarations: [...components],
   exports: [...components],
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule],
 })
 export class WidgetsModule {}
